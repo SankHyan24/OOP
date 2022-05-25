da245266f754834600209f40ca0eb12a2a0d64eb
Charles Sun <2209339564@qq.com>
2022-05-25 09:33:20 +0800

    refine cmake configure

--- oopx/test/include/test.hpp
+++ oopx/test/include/test.hpp
@@ -1,7 +1,9 @@
 #pragma once
+// test utils
 #include <assert.h>
 #define ASSERT(MESSAGE, VALUE) assert((VALUE) && (MESSAGE))
 #define ASSERT_EQ(VALUE_1, VALUE_2) assert((VALUE_1) == (VALUE_2))
 #define ASSERT_NE(VALUE_1, VALUE_2) assert((VALUE_1) != (VALUE_2))
 // declare the test subfunction which will nbe used in the test_main function
 void binary_test();
+void xml_test();
---
+++ oopx/test/src/xmltest.cpp
@@ -0,0 +1,16 @@
+#include <xmlserial.hpp>
+#include <utility.hpp>
+#include <test.hpp>
+
+#include <climits>
+#include <random>
+#include <map>
+#include <list>
+#include <vector>
+#include <set>
+using namespace std;
+void xml_test()
+{
+
+    cout << "Test Done" << endl;
+}
